7550cb90a9662379cdb0760b0f4c969ba3aeaa70
rohit <rebelindia01@gmail.com>
2024-10-06 20:51:12 +0530

    hero and features sections added

---
+++ src/components/features.jsx
@@ -0,0 +1,39 @@
+import React from 'react';
+import FeatureCard from './FeatureCard';
+
+
+
+const features = [
+  {
+    image: '/public/download.jpeg',
+    title: 'Order Online',
+    description: 'Stay home and order to your doorstep',
+  },
+  {
+    image: 'download.jpeg',
+    title: 'Dining',
+    description: 'View the city\'s favorite dining venues',
+  },
+  {
+    image: 'download.jpeg',
+    title: 'Nightlife and Clubs',
+    description: 'Explore the city\'s top nightlife outlets',
+  },
+];
+
+const FeatureCards = () => {
+  return (
+    <div className="flex justify-center flex-wrap gap-10">
+      {features.map((feature, index) => (
+        <FeatureCard
+          key={index}
+          image={feature.image}
+          title={feature.title}
+          description={feature.description}
+        />
+      ))}
+    </div>
+  );
+};
+
+export default FeatureCards;
