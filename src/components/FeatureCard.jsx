2508aa10eadd52d29584d8cf0c924b586b142427
tasin-dev-pro <tasinprodev@gmail.com>
2024-10-08 20:07:00 +0600

    header fixed

--- src/components/FeatureCard.jsx
+++ src/components/FeatureCard.jsx
@@ -1,16 +1,17 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
 
 const FeatureCard = ({ image, title, description }) => {
   return (
-    <div className="w-[300px] rounded overflow-hidden shadow-lg hover:shadow-2xl transition-all duration-300">
+    <Link to={title === 'Dining' ? '/restaurants' : '/foods'}  className="w-[300px] rounded overflow-hidden shadow-lg hover:shadow-2xl transition-all duration-300">
       <img className="w-full h-44" src={image} alt={title} />
       <div className="px-6 py-4">
-        <a className="font-bold text-xl mb-2 underline text-red-400" href={title === 'Dining' ? '/restaurants' : '/foods'}>{title}</a>
+        <h1 className="font-bold text-xl mb-2 underline text-red-400">{title}</h1>
         <p className="text-gray-700 text-base">
           {description}
         </p>
       </div>
-    </div>
+    </Link>
   );
 };
 
